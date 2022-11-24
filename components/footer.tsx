ef3bcbbb8da8f54e3179b1b465d930814ac3c8c9
PegasusMKD <csprogrammers@outlook.com>
2022-11-24 04:48:56 +0100

    Bugfix hydration issues

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,8 +6,9 @@ export default function Footer() {
         <footer>
             <hr className={"my-6 border-neutral-200 sm:mx-auto dark:border-neutral-700 lg:my-8"}/>
             <span className={"block text-sm text-neutral-500 sm:text-center dark:text-neutral-400 pb-8"}>
+                © 2022{' '}
                 <Link href={'/'}>
-                    © 2022 <a className={"hover:underline underline-offset-2"}>FAK Studios™</a>.{' '}
+                     <span className={"hover:underline underline-offset-2"}>FAK Studios™</span>.{' '}
                 </Link>
                 All Rights Reserved.
             </span>
