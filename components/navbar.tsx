2b27f49c34889f1f0222c30feb361c17f5feeb5a
PegasusMKD <csprogrammers@outlook.com>
2022-12-11 17:46:07 +0100

    Make popover a bit more usable

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,13 @@
 import NavItem from "./nav-item";
-import UserNavBar from "./user-nav-bar";
+import UserNavBar, {openAtom} from "./user-nav-bar";
+import {useAtom} from "jotai";
 
 export default function Navbar() {
+    const [_1, setOpen] = useAtom(openAtom);
+
     return (
         <nav className={"flex flex-row w-full bg-neutral-900 border-4 border-neutral-900 border-b-neutral-800"}>
-            <ul className={"flex list-none pl-8 py-1 basis-5/6 text-neutral"}>
+            <ul className={"flex list-none pl-8 py-1 basis-5/6 text-neutral"} onClick={() => setOpen(false)} >
                 <NavItem href={'/'} text={"Home"}/>
                 <NavItem href={'/leaderboards'} text={"Leaderboards"}/>
                 <NavItem href={'/about'} text={"About Us"}/>
---
+++ pages/api/attempts.ts
@@ -0,0 +1,12 @@
+import {NextApiRequest, NextApiResponse} from "next";
+
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+
+    // This should serve to access previous
+    //  game attempts for a specific user so we can display it on the FE - profile - history
+
+}
