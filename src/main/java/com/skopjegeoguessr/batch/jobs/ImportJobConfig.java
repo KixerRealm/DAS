d4ec47b62387f4869d7e0670b65c190ee2347de9
PegasusMKD <csprogrammers@outlook.com>
2022-12-21 18:17:21 +0100

    Make job parameterized

--- src/main/java/com/skopjegeoguessr/batch/jobs/ImportJobConfig.java
+++ src/main/java/com/skopjegeoguessr/batch/jobs/ImportJobConfig.java
@@ -6,7 +6,6 @@ import com.google.gson.Gson;
 import com.google.maps.PlacesApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.PlacesSearchResponse;
-import com.google.maps.model.PlacesSearchResult;
 import com.skopjegeoguessr.batch.config.processors.PlaceProcessor;
 import com.skopjegeoguessr.batch.places.PlaceMapper;
 import com.skopjegeoguessr.batch.places.model.Place;
@@ -27,18 +26,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.FileSystemResource;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class CoffeeJobConfig {
+public class ImportJobConfig {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
@@ -51,69 +50,67 @@ public class CoffeeJobConfig {
 
 	@Lazy
 	@Autowired
-	@Qualifier("CoffeeReader")
+	@Qualifier("ImportReader")
 	private JsonItemReader<Place> reader;
 
 	@Lazy
 	@Autowired
-	@Qualifier("CoffeeStep")
-	private Step coffeeStep;
+	@Qualifier("ImportStep")
+	private Step importStep;
 
 	@Bean
-	public Job coffeeJob() {
-		return jobBuilderFactory.get("coffeeJob").start(coffeeStep).build();
+	public Job importJob() {
+		return jobBuilderFactory.get("importJob").start(importStep).build();
 	}
 
-	@Bean(name = "CoffeeStep")
-	public Step coffeeStep() {
-		return stepBuilderFactory.get("CoffeeStep")
+	@Bean(name = "ImportStep")
+	public Step importStep() {
+		return stepBuilderFactory.get("ImportStep")
 				.<Place, Place>chunk(10)
 				.reader(reader).processor(processor).writer(writer)
 				.build();
 	}
 
-	@Bean(name = "CoffeeReader")
-	public JsonItemReader<Place> coffeeItemReader() throws IOException, InterruptedException, ApiException {
+	@Scope("step")
+	@Bean(name = "ImportReader")
+	public JsonItemReader<Place> importItemReader(
+			@Value("#{jobParameters['query']}") String query,
+			@Value("#{jobParameters['result-location']}") String location
+	) throws IOException, InterruptedException, ApiException {
+
+		log.info("Called places job using query '{}'", query);
 		final ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		final JacksonJsonObjectReader<Place> jsonObjectReader = new JacksonJsonObjectReader<>(Place.class);
 		jsonObjectReader.setMapper(mapper);
 		PlacesSearchResponse results = utilityService.wrapWithContext(context ->
-				PlacesApi.textSearchQuery(
-						context,
-						"coffeeshops+skopje+macedonia"
-				).await());
+				PlacesApi.textSearchQuery(context, query).await()
+		);
 
 		List<PlacesSearchResponse> responses = new java.util.ArrayList<>(List.of(results));
-		Thread.sleep(3000);
 		while (responses.get(responses.size() - 1).nextPageToken != null &&
 				responses.get(responses.size() - 1).nextPageToken.length() != 0) {
 			Thread.sleep(3000);
 			responses.add(
 					utilityService.wrapWithContext(context ->
-							PlacesApi.textSearchQuery(
-									context,
-									"coffeeshops+skopje+macedonia"
-							).pageToken(responses.get(responses.size() - 1).nextPageToken).await()
+							PlacesApi.textSearchQuery(context, query)
+									.pageToken(responses.get(responses.size() - 1).nextPageToken)
+									.await()
 					)
 			);
 		}
-		List<PlacesSearchResult[]> responses1 = new ArrayList<>();
-		for (PlacesSearchResponse p : responses) {
-			responses1.add(p.results);
-		}
-		List<PlacesSearchResult> responses2 = new ArrayList<>();
-		for (PlacesSearchResult[] p : responses1) {
-			responses2.addAll(Arrays.asList(p));
-		}
 
-		List<Place> placeList = responses2.stream().map(PlaceMapper.INSTANCE::toEntity).toList();
+		List<Place> placeList = responses.stream()
+				.flatMap(p -> Arrays.stream(p.results))
+				.map(PlaceMapper.INSTANCE::toEntity).toList();
 
-		FileWriter writer = new FileWriter("src/main/resources/static/results.json");
+		FileWriter writer = new FileWriter(location);
 		gson.toJson(placeList, writer);
 		writer.flush();
+
 		return new JsonItemReaderBuilder<Place>().jsonObjectReader(jsonObjectReader)
-				.resource(new FileSystemResource("src/main/resources/static/results.json"))
+				.resource(new FileSystemResource(location))
 				.name("jsonItemReader")
 				.build();
 	}
 }
+
--- src/main/java/com/skopjegeoguessr/batch/places/PlaceController.java
+++ src/main/java/com/skopjegeoguessr/batch/places/PlaceController.java
@@ -7,8 +7,6 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,20 +19,27 @@ public class PlaceController {
 
 	private final JobLauncher jobLauncher;
 
-	@Autowired
-	@Qualifier("coffeeJob")
-	private Job job;
-
-	@Autowired
-	@Qualifier("landmarkJob")
-	private Job job1;
+	private final Job importJob;
 
 	@PostMapping("/coffee")
-	public void importJsonToDBJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+	public void coffeeJobStart() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
 		JobParameters jobParameters = new JobParametersBuilder()
 				.addLong("startAt", System.currentTimeMillis())
+				.addString("query", "coffeeshops+skopje+macedonia")
+				.addString("result-location", "src/main/resources/static/results.json")
 				.toJobParameters();
-		JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+		JobExecution jobExecution = jobLauncher.run(importJob, jobParameters);
+		log.info("Job Execution: " + jobExecution.getStatus());
+	}
+
+	@PostMapping("/landmark")
+	public void landmarkJobStart() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addLong("startAt", System.currentTimeMillis())
+				.addString("query", "landmarks+skopje+macedonia")
+				.addString("result-location", "src/main/resources/static/results1.json")
+				.toJobParameters();
+		JobExecution jobExecution = jobLauncher.run(importJob, jobParameters);
 		log.info("Job Execution: " + jobExecution.getStatus());
 	}
 }
